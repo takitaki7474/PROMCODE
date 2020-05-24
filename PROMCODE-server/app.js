0601589a884c46add4cff6810e279e521c8cdcd7
oota <shark747474@gmail.com>
2020-05-24 23:09:55 +0900

    post test

--- PROMCODE-server/app.js
+++ PROMCODE-server/app.js
@@ -1,9 +1,11 @@
-
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.listen(3000);
 console.log('Server is online.');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.send('Hello World\n');
