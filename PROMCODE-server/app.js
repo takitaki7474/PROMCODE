75fcd93e1b4b019f59bc9fcbdf03610e52e35f88
oota <shark747474@gmail.com>
2020-05-24 18:37:29 +0900

    add PROMCODE-server

--- PROMCODE-server/app.js
+++ PROMCODE-server/app.js
@@ -1,11 +1,9 @@
+
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.listen(3000);
 console.log('Server is online.');
-
-//app.use(express.static(path.join(__dirname, 'turtle_sample')));
 
 app.get('/', function(req, res) {
   res.send('Hello World\n');
