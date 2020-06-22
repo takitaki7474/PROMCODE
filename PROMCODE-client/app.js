d67f8d596ce353d4aa39afaf313a301906c02812
oota <shark747474@gmail.com>
2020-06-22 22:18:10 +0900

    add scopeitem-resource.ttl

--- PROMCODE-client/app.js
+++ PROMCODE-client/app.js
@@ -1,2 +1,5 @@
 // post
 // curl http://localhost:3000 -F "file=@upload/neo4j-sample.ttl" -F filename=neo4j-sample.ttl
+// curl http://localhost:3000 -F "file=@upload/project-resource.ttl" -F filename=project-resource.ttl
+// curl http://localhost:3000 -F "file=@upload/scopeitem-container.ttl" -F filename=scopeitem-container.ttl
+// curl http://localhost:3000 -F "file=@upload/scopeitem-resource.ttl" -F filename=scopeitem-resource.ttl
