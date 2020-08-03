410e6c72f61822f58f254942f581750085edf268
oota <shark747474@gmail.com>
2020-08-03 22:35:00 +0900

    Add file selection button

---
+++ PROMCODE-client/src/components/FileSelectionBtn.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="file-selection-button">
+    <input type="file" name="ファイルを選択">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileSelectionBtn",
+}
+</script>
+
+<style scoped>
+</style>
